9903eb6d331cf2d6d81b9c79bbc1c5fa9726bfee
Andreas Bjoru <andreas.bjoru@gmail.com>
2026-02-19 21:41:59 -0500

    add dotf formula (pre-built macOS ARM64 binary)

---
+++ Formula/dotf.rb
@@ -0,0 +1,19 @@
+class Dotf < Formula
+  desc "Modular dotfile manager using git bare repo with sparse checkout"
+  homepage "https://github.com/abjoru/dotf-v3"
+  version "3.0.0.0"
+  url "https://github.com/abjoru/dotf-v3/releases/download/v#{version}/dotf-v#{version}-macos-arm64"
+  sha256 "PLACEHOLDER"
+  license "BSD-3-Clause"
+
+  depends_on :macos
+  depends_on "git"
+
+  def install
+    bin.install "dotf-v#{version}-macos-arm64" => "dotf"
+  end
+
+  test do
+    system "#{bin}/dotf", "--help"
+  end
+end
