39aa2419cf03bfbcb2e017fd0aec939f74343556
Andreas Bjoru <andreas.bjoru@gmail.com>
2026-02-20 20:47:47 -0500

    fix(dotf): update sha256 for v3.0.2

--- Formula/dotf.rb
+++ Formula/dotf.rb
@@ -3,7 +3,7 @@ class Dotf < Formula
   homepage "https://github.com/abjoru/dotf-v3"
   version "3.0.2"
   url "https://github.com/abjoru/dotf-v3/releases/download/v#{version}/dotf-v#{version}-macos-arm64"
-  sha256 "PLACEHOLDER"
+  sha256 "b72f0f3e355c54a01a9f5be1bdef8c9014e98b07890955f8a9ba9169da76376c"
   license "BSD-3-Clause"
 
   depends_on :macos
