ee5f3e1b2a17d0bdbad1f73a0149b8c1c757e1e8
Andreas Bjoru <andreas.bjoru@gmail.com>
2026-02-20 19:35:14 -0500

    chore(dotf): bump to v3.0.1

--- Formula/dotf.rb
+++ Formula/dotf.rb
@@ -1,9 +1,9 @@
 class Dotf < Formula
   desc "Modular dotfile manager using git bare repo with sparse checkout"
   homepage "https://github.com/abjoru/dotf-v3"
-  version "3.0.0"
+  version "3.0.1"
   url "https://github.com/abjoru/dotf-v3/releases/download/v#{version}/dotf-v#{version}-macos-arm64"
-  sha256 "3ee282938b9db6d7404509cc94cd17982bc136e170214db114966be2d998b38e"
+  sha256 "f49740f6c0bc2645615a96c4c3cae136b57197a8eef5594efb672d2c0ae9e9c3"
   license "BSD-3-Clause"
 
   depends_on :macos
